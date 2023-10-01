b093f6f8857a0f5c5995f612c7edcdf4c5deef50
00codester <00codester@gmail.com>
2023-09-30 22:48:35 -0600

    lesson 3 localhost side functioning

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,68 @@ const getSingle = async(req, res, next) => {
     });
 };
 
-module.exports = { getAll, getSingle };
+//fname, lname, birthdayIn, emailIn, favColor
+const postContact = async(req, res) => {
+    //const userId = new ObjectId(fname, lname, birthdayIn, email, favColor);
+    const newContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    // const myDB = client.db();
+    // const myColl = myDB.collection('contacts');
+    // //birthday: birthdayIn, email: emailIn, favoriteColor: favColor, firstName: fname, lastName: lname
+    // const doc = { birthday: "12/21/18", email: "gohome@gmail.com", favoriteColor: "Violet", firstName: "Cory", lastName: "Karl" };
+    // const result = await myColl.insertOne(doc);
+    // console.log(
+    //    `A document was inserted with the _id: ${result.insertedId}`,
+    // );
+    const result = await mongodb.getDb().db().collection('contacts').insertOne(newContact);
+    if(result.acknowledged){
+        res.status(201).json(result);
+    } else {
+        res.status(500).json(response.error || 'Error occured while creating contact');
+    }
+};
+const updateContact = async(req, res) =>{
+    const userId = new ObjectId(req.params.id);
+    
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const result = await mongodb.getDb().db().collection('contacts').replaceOne({_id: userId}, contact);
+    console.log(result);
+    if(result.modifiedCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Error occured while creating contact');
+    }
+
+};
+
+const deleteContact = async(req, res) =>{
+    const userId = new ObjectId(req.params.id);
+    
+    const result = await mongodb.getDb().db().collection('contacts').deleteOne({_id: userId}, true);
+    console.log(result);
+    if(result.deletedCount > 0){
+        res.status(200).send();
+    } else {
+        res.status(500).json(response.error || 'Error occured while creating contact');
+    }
+
+};
+
+module.exports = { 
+    getAll, 
+    getSingle, 
+    postContact, 
+    updateContact, 
+    deleteContact 
+};
